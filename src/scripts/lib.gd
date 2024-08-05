598f7b8ef558c6f37ae357728ae9bc925de02d6d
Gabor Heja <gheja@ubuntu1.w830g8.git.stdio.hu>
2024-08-06 00:08:05 +0200

    Added lib

---
+++ src/scripts/lib.gd
@@ -0,0 +1,11 @@
+extends Node
+
+func get_first_group_member(group_name):
+	var members = get_tree().get_nodes_in_group(group_name)
+	
+	if members.size() == 0:
+		print("Warning: No object matching group \"", group_name, "\", returning null.")
+		return null
+	
+	return members[0]
+
