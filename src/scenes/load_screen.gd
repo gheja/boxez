ec8800e7a8fbbd075f38972c8f000b432dca8bae
Gabor Heja <gheja@ubuntu1.w830g8.git.stdio.hu>
2024-08-15 14:32:07 +0200

    The load screen is now skipped on Windows

--- src/scenes/load_screen.gd
+++ src/scenes/load_screen.gd
@@ -1,6 +1,13 @@
 extends Node2D
 
+func go_to_menu():
+	AudioManager.start_menu_music()
+	get_tree().change_scene_to_file("res://scenes/menu_screen.tscn")
+
+func _ready():
+	if OS.get_name() == "Windows":
+		go_to_menu.call_deferred()
+
 func _on_color_rect_gui_input(event):
 	if event is InputEventMouseButton:
-		AudioManager.start_menu_music()
-		get_tree().change_scene_to_file("res://scenes/menu_screen.tscn")
+		go_to_menu()
